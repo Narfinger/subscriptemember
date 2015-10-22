74e4bf6465111f6adc765b3ca4a692c64eb8c992
Narfinger <Narfinger@users.noreply.github.com>
2015-10-22 21:48:56 +0200

    some changes

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP, DeriveDataTypeable, FlexibleContexts,
   GeneralizedNewtypeDeriving, MultiParamTypeClasses,
-  TemplateHaskell, TypeFamilies, RecordWildCards, OverloadedStrings #-}
+  TemplateHaskell, TypeFamilies, RecordWildCards, OverloadedStrings,
+  StandaloneDeriving #-}
 
 module Main where
 
@@ -22,16 +23,45 @@ import Data.Acid.Advanced   ( query', update' )
 import Data.Acid.Local      ( createCheckpointAndClose )
 import Data.SafeCopy        ( base, deriveSafeCopy )
 import Network.URI
+import Data.Text                     (Text)
+import qualified Network.HTTP.Conduit as C
+import Data.Aeson                    (FromJSON)
+import Data.Aeson.TH                 (defaultOptions, deriveJSON)
+import           Keys                          (googleKey)
+import           Network.OAuth.OAuth2
+import qualified Data.ByteString.Char8         as BS
+
 
 data YoutubeVideo = YoutubeVideo { title :: String
                                  , url :: String
                                  } deriving (Eq, Ord, Read, Show, Data, Typeable)
+deriving instance Eq AccessToken
+deriving instance Ord AccessToken
+deriving instance Read AccessToken
+deriving instance Data AccessToken
+
+googleScopeUserInfo :: QueryParams
+googleScopeUserInfo = [("scope", "https://www.googleapis.com/auth/userinfo.profile")]
+-- data Token = Token { issued_to   :: Text
+--                    , audience    :: Text
+--                    , user_id     :: Maybe Text
+--                    , scope       :: Text
+--                    , expires_in  :: Integer
+--                    -- , email          :: Maybe Text
+--                    -- , verified_email :: Maybe Bool
+--                    , access_type :: Text
+--                    } deriving (Eq, Ord, Read, Show, Data, Typeable)
+
+$(deriveSafeCopy 0 'base ''AccessToken)
+-- $(deriveJSON defaultOptions ''AccessToken)
 
 data ServerState = ServerState { videos :: [YoutubeVideo]
+                               , token :: Maybe AccessToken
                                } deriving (Eq, Ord, Read, Show, Data, Typeable)
 
 $(deriveSafeCopy 0 'base ''YoutubeVideo)
 $(deriveSafeCopy 0 'base ''ServerState)
+
 
 tmpdata :: [YoutubeVideo]
 tmpdata = [ YoutubeVideo "test1" "http://test"
@@ -41,7 +71,17 @@ tmpdata = [ YoutubeVideo "test1" "http://test"
   
 initialServerState :: ServerState
 initialServerState = ServerState { videos = tmpdata
-                                 }
+                                   , token = Nothing
+                                   }
+
+getToken :: C.Manager -> IO AccessToken
+getToken mgr = do
+    BS.putStrLn $ authorizationUrl googleKey `appendQueryParam` googleScopeUserInfo
+    putStrLn "visit the url and paste code here: "
+    code <- fmap BS.pack getLine
+    (Right token) <- fetchAccessToken mgr googleKey code
+    return token
+
 appendVideos :: YoutubeVideo -> Update ServerState [YoutubeVideo]
 appendVideos v = do
   vs@ServerState{..} <- get
@@ -52,7 +92,16 @@ appendVideos v = do
 getVideos :: Query ServerState [YoutubeVideo]
 getVideos = videos <$> ask
 
-$(makeAcidic ''ServerState ['appendVideos, 'getVideos])
+getAccessToken :: Query ServerState (Just AccessToken)
+getAccessToken = token <$> ask
+
+writeAccessToken :: AccessToken -> Update ServerState AccessToken
+writeAccessToken tk = do
+  vs@ServerState{..} <- get
+  put $ vs { token = Just tk }
+  return tk
+
+$(makeAcidic ''ServerState ['appendVideos, 'getVideos, 'getAccessToken, 'writeAccessToken])
 
 
 bodyTemplate :: H.Html ->H.Html
@@ -78,10 +127,12 @@ videoTemplate v =
     H.td $ do H.toHtml $ title v
     H.td $ do H.toHtml $ url v
 
-indexPage :: [YoutubeVideo] -> H.Html
-indexPage vs = bodyTemplate $
-               H.table ! A.class_ "table table-striped" $ do
-                 mapM_ videoTemplate vs
+indexPage :: [YoutubeVideo] -> AccessToken -> H.Html
+indexPage vs tk = bodyTemplate $
+                  H.table ! A.class_ "table table-striped" $ do
+                    H.tr $ do
+                      H.td $ do H.toHtml $ show tk
+                    mapM_ videoTemplate vs
 
 
 handlers :: AcidState ServerState -> ServerPart Response
@@ -89,15 +140,24 @@ handlers acid = msum
   [
     do nullDir
        vs <- query' acid GetVideos
-       ok $ toResponse $ indexPage vs
+       tk <- query' acid GetAccessToken
+       ok $ toResponse $ indexPage vs tk
   ]
-
-
-
  
 main :: IO ()
-main =
-  bracket (openLocalState initialServerState)
+main = do
+  -- this whole thing needs to be done only if no state was found
+  mgr <- C.newManager C.conduitManagerSettings
+  token <- getToken mgr
+  C.closeManager mgr
+  bracket (openLocalState (initialServerState token))
           (createCheckpointAndClose)
-           (\acid ->
-               simpleHTTP nullConf (handlers acid))
+         (\acid -> do 
+              tk <- query' acid GetAccessToken
+              when (isNothing tk) do {
+                mgr <- C.newManager C.conduitManagerSettings;
+                token <- getToken mgr;
+                C.closeManager mgr;
+                update' acid (WriteAccessToken token)
+                }
+              simpleHTTP nullConf (handlers acid))
