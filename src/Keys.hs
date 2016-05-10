b1ce0277e2bd43dc136322258ec46d0b57a220c7
Narfinger <Narfinger@users.noreply.github.com>
2016-05-10 22:15:56 +0900

    some changes to keyfile
    and of course invalidated the old keys (repository was never public when
    these where still active)

--- src/Keys.hs
+++ src/Keys.hs
@@ -5,9 +5,11 @@ module Keys where
 import           Network.OAuth.OAuth2
 
 -- | oauthCallback = Just "https://developers.google.com/oauthplayground"
+
+-- If you look into my repository to get the keys, tough luck, I changed them
 googleKey :: OAuth2
-googleKey = OAuth2 { oauthClientId = "270774910181-g7hj6igbi6tqskqjjk2vibheuoor6vut.apps.googleusercontent.com"
-                   , oauthClientSecret = "34SzUVlFUeXxX1ia9-kOzAgy"
+googleKey = OAuth2 { oauthClientId = "XXXXXX.apps.googleusercontent.com"
+                   , oauthClientSecret = "XXX"
                    , oauthCallback = Just "http://127.0.0.1:9988/googleCallback"
                    , oauthOAuthorizeEndpoint = "https://accounts.google.com/o/oauth2/auth"
                    , oauthAccessTokenEndpoint = "https://www.googleapis.com/oauth2/v3/token"
