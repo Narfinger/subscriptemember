ec481f20849bb85d28f4f9f745b21bb569370dab
Narfinger <Narfinger@users.noreply.github.com>
2016-12-27 15:10:40 +0100

    stuff

--- src/WebsocketsHandler.hs
+++ src/WebsocketsHandler.hs
@@ -12,7 +12,8 @@
 module WebsocketsHandler (newWSState, writeConnection, updateWSClients, WSState) where
 
 import           Control.Concurrent   (MVar, modifyMVar, modifyMVar_, newMVar,
-                                       putMVar, readMVar)
+                                       putMVar, readMVar, swapMVar)
+import           Data.Maybe           (isNothing)
 import           Data.Text            (Text (..))
 import qualified Network.HTTP.Conduit as C
 import           Network.OAuth.OAuth2
@@ -26,11 +27,14 @@ newWSState = newMVar Nothing
 
 writeConnection :: WSState -> WS.Connection -> IO ()
 writeConnection state conn = do
-  putMVar state (Just conn)
+  swapMVar state (Just conn)
+  return ()
 
 updateWSClients :: WSState -> IO ()
 updateWSClients wsstate = do
   state <- readMVar wsstate
+  putStrLn "test"
+  putStrLn $ show $ isNothing state
   case state of
     Nothing -> return ()
     Just conn -> WS.sendTextData conn ("update" :: Text)
