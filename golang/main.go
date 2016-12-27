374dd1be51a9bfab4095f8327de0ed30d461d2fb
Narfinger <Narfinger@users.noreply.github.com>
2016-12-27 21:32:21 +0100

    some changes

--- golang/main.go
+++ golang/main.go
@@ -2,23 +2,53 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"bufio"
+	"os"
+	
+	"log"
+	"net/http"
+
+	"lib"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	youtube "google.golang.org/api/youtube/v3"
 )
 
-func main() {
+func firstOAuthSetup() *http.Client {
 	fmt.Println("test")
-	dat, err := ioutil.ReadFile("~/client_secret.json")
-	fmt.Println(err)
-	conf, err := google.ConfigFromJSON(dat)
-	fmt.Println(err)
 
 	url := conf.AuthCodeURL("state")
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-	tok, err := conf.Exchange(oauth2.NoContext, "authorization-code")
-	client := conf.Client(oauth2.NoContext, tok)
+	fmt.Printf("Visit the URL for the auth dialog:\n %v\n", url)
 
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter code: ")
+	text, _ := reader.ReadString('\n')
+	
+	
+	tok, err := conf.Exchange(oauth2.NoContext, text)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("this is a test")
+	return conf.Client(oauth2.NoContext, tok)
+	
+}
+
+func main() {
+	client := firstOAuthSetup()
+	
 	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	subslistcall := youtube.NewSubscriptionsService(service).List("contentDetails")
+	subs, err := subslistcall.Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(subs)
+	for _,elem := range subs.Items {
+		fmt.Println(elem.Etag)
+	}
+	
 }
