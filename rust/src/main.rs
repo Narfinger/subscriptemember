f596fb1028b990b0e99d4a2b16096533f632bb58
Narfinger <Narfinger@users.noreply.github.com>
2017-01-19 01:32:47 +0900

    rustfmt

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,24 +2,30 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 #[cfg(feature = "nightly")]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate hyper;
 extern crate yup_oauth2 as oauth2;
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
 extern crate rocket;
 extern crate handlebars;
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_codegen;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
 extern crate dotenv;
 
 pub mod youtube_handler;
 pub mod schema;
 
 use std::sync::Mutex;
-use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ConsoleApplicationSecret, DiskTokenStorage, GetToken,};
+use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ConsoleApplicationSecret,
+             DiskTokenStorage, GetToken};
 use serde_json as json;
 use std::io::prelude::*;
 use std::fs::File;
@@ -41,27 +47,29 @@ lazy_static! {
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-fn setup_oauth() -> Result<oauth2::Token, Box<std::error::Error>> { 
+fn setup_oauth() -> Result<oauth2::Token, Box<std::error::Error>> {
     let mut f = File::open("client_secret.json").expect("Did not find client_secret.json");
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
-    
+
     let secret = json::from_str::<ConsoleApplicationSecret>(&s).unwrap().installed.unwrap();
     let mut cwd = std::env::current_dir().unwrap();
     cwd.push("tk");
-    let cwd : String = String::from(cwd.to_str().expect("string conversion error"));
+    let cwd: String = String::from(cwd.to_str().expect("string conversion error"));
     println!("{}", cwd);
     let ntk = DiskTokenStorage::new(&cwd).expect("disk storage token is broken");
-    
-    Authenticator::new(&secret, DefaultAuthenticatorDelegate,
+
+    Authenticator::new(&secret,
+                       DefaultAuthenticatorDelegate,
                        hyper::Client::new(),
-                       ntk, None).token(&["https://www.googleapis.com/auth/youtube"])
+                       ntk,
+                       None)
+        .token(&["https://www.googleapis.com/auth/youtube"])
 }
 
 #[get("/updateSubs")]
@@ -77,7 +85,7 @@ fn subs() -> String {
     let sub = youtube_handler::get_subs(&TK, &DB, false);
     let mut data = BTreeMap::new();
     data.insert("subs".to_string(), sub.to_json());
-    data.insert("numberofsubs".to_string(),sub.len().to_json());
+    data.insert("numberofsubs".to_string(), sub.len().to_json());
     HB.lock().unwrap().render("subs", &data).unwrap()
 }
 
@@ -90,16 +98,16 @@ fn index() -> String {
     HB.lock().unwrap().render("index", &data).unwrap()
 }
 
-fn template_filename_to_string(s : &str) -> Result<String,String> {
+fn template_filename_to_string(s: &str) -> Result<String, String> {
     let mut f = match File::open(s) {
         Ok(ff) => ff,
-        Err(e) => return Err(e.to_string())
+        Err(e) => return Err(e.to_string()),
     };
-    
+
     let mut s = String::new();
     match f.read_to_string(&mut s) {
         Ok(br) => br,
-        Err(e) => return Err(e.to_string())
+        Err(e) => return Err(e.to_string()),
     };
     Ok(s)
 }
@@ -109,22 +117,22 @@ fn main() {
     // println!("storing token to disk");
     // let mut st = DiskTokenStorage("tk");
     // st.set(1,["https://www.googleapis.com/auth/youtube"],t);
-//    let tk = setup_oauth();
+    //    let tk = setup_oauth();
 
     println!("DONE!!!");
     println!("Registering templates");
     {
         let its = template_filename_to_string("templates/index.html").unwrap();
-        assert!(HB.lock().unwrap().register_template_string("index",its ).is_ok());
-        
+        assert!(HB.lock().unwrap().register_template_string("index", its).is_ok());
+
         let its = template_filename_to_string("templates/subs.html").unwrap();
-        assert!(HB.lock().unwrap().register_template_string("subs",its ).is_ok());
+        assert!(HB.lock().unwrap().register_template_string("subs", its).is_ok());
     }
-       
-    
-    println!("Starting server"); 
-    rocket::ignite().mount("/", routes![update_subs,subs,index]).launch();
-        
+
+
+    println!("Starting server");
+    rocket::ignite().mount("/", routes![update_subs, subs, index]).launch();
+
 
     // now you can use t.access_token to authenticate API calls within your
     // given scopes. It will not be valid forever, but Authenticator will automatically
--- rust/src/youtube_handler.rs
+++ rust/src/youtube_handler.rs
@@ -8,15 +8,17 @@ use serde;
 use serde_json;
 use diesel::sqlite::SqliteConnection;
 use diesel::prelude::*;
-use diesel::{insert,delete};
+use diesel::{insert, delete};
 use schema::subscriptions;
 
-const SUB_URL:&'static str = "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&maxResults=50&access_token=";
+const SUB_URL: &'static str = "https://www.googleapis.\
+                               com/youtube/v3/subscriptions\
+                               ?part=snippet&mine=true&maxResults=50&access_token=";
 
 #[derive(Serialize, Deserialize)]
 struct YoutubePageInfo {
     #[serde(rename="totalResults")]
-    total_results : i32,
+    total_results: i32,
 
     #[serde(rename="resultsPerPage")]
     results_per_page: i32,
@@ -24,56 +26,53 @@ struct YoutubePageInfo {
 
 #[derive(Serialize, Deserialize)]
 struct YoutubeResult<T> {
-    items : Vec<YoutubeItem<T>>,
+    items: Vec<YoutubeItem<T>>,
     #[serde(rename="pageInfo")]
-    page_info : YoutubePageInfo,
+    page_info: YoutubePageInfo,
     #[serde(rename="nextPageToken")]
-    next_page_token : Option<String>
+    next_page_token: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct YoutubeItem<T> {
     #[serde(rename="id")]
-    iid : String,
-    snippet : Option<T>,
-    content_details : Option<T>
+    iid: String,
+    snippet: Option<T>,
+    content_details: Option<T>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct YoutubeThumbnailDetail {
     #[serde(rename="url")]
-    thmburl : String
+    thmburl: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct YoutubeThumbnails {
-    default : YoutubeThumbnailDetail,
-    medium : YoutubeThumbnailDetail,
-    high : YoutubeThumbnailDetail
+    default: YoutubeThumbnailDetail,
+    medium: YoutubeThumbnailDetail,
+    high: YoutubeThumbnailDetail,
 }
 
 #[derive(Serialize, Deserialize)]
 struct YoutubeSubscription {
     #[serde(rename="title")]
-    subscription_title : String,
+    subscription_title: String,
     #[serde(rename="description")]
-    sdescription : String,
+    sdescription: String,
     #[serde(rename="channelId")]
-    channel_id : String,
-    thumbnails : YoutubeThumbnails
-    
-    // resourceId : YoutubeResource,
-    // thumbnails : YoutubeThumbnails,
-
+    channel_id: String,
+    thumbnails: YoutubeThumbnails, /* resourceId : YoutubeResource,
+                                    * thumbnails : YoutubeThumbnails */
 }
 
 #[derive(Eq,PartialEq,PartialOrd,Ord,Debug,Hash,Serialize,Deserialize,Queryable)]
 pub struct Subscription {
-    pub sid : i32,
-    pub channelname : String,
-    pub upload_playlist : String,
-    pub thumbnail : String,
-    pub description : String,
+    pub sid: i32,
+    pub channelname: String,
+    pub upload_playlist: String,
+    pub thumbnail: String,
+    pub description: String,
 }
 
 #[derive(Insertable)]
@@ -87,13 +86,23 @@ struct NewSubscription {
 
 impl fmt::Display for Subscription {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "(sid: {}, chnn: {}, uplpl: {}, thmb: {})", self.sid, self.channelname, self.upload_playlist, self.thumbnail)
+        write!(f,
+               "(sid: {}, chnn: {}, uplpl: {}, thmb: {})",
+               self.sid,
+               self.channelname,
+               self.upload_playlist,
+               self.thumbnail)
     }
 }
 
-fn query_simple_page<T>(t : &oauth2::Token, url : &'static str, nextpage : Option<String>) -> YoutubeResult<T> where T: serde::Deserialize {
+fn query_simple_page<T>(t: &oauth2::Token,
+                        url: &'static str,
+                        nextpage: Option<String>)
+                        -> YoutubeResult<T>
+    where T: serde::Deserialize
+{
     let client = Client::new();
-    let mut q  = String::from(url);
+    let mut q = String::from(url);
     q.push_str(t.access_token.as_str());
     if let Some(nextpagetk) = nextpage {
         q.push_str("&pageToken=");
@@ -101,7 +110,7 @@ fn query_simple_page<T>(t : &oauth2::Token, url : &'static str, nextpage : Optio
     }
 
     let res = client.get(q.as_str()).send().unwrap();
-//    res.read_to_string(&mut s);
+    //    res.read_to_string(&mut s);
     println!("query: {}", q);
 
     serde_json::from_reader(res).unwrap()
@@ -111,40 +120,53 @@ fn query_simple_page<T>(t : &oauth2::Token, url : &'static str, nextpage : Optio
     // s
 }
 
-fn query<T>(t : &oauth2::Token, url : &'static str) -> Vec<YoutubeItem<T>> where T : serde::Deserialize {
-    let mut result : Vec<YoutubeItem<T>> = Vec::new();
+fn query<T>(t: &oauth2::Token, url: &'static str) -> Vec<YoutubeItem<T>>
+    where T: serde::Deserialize
+{
+    let mut result: Vec<YoutubeItem<T>> = Vec::new();
     let mut next_page = None;
     loop {
-        let res = query_simple_page(t,url,next_page);
+        let res = query_simple_page(t, url, next_page);
         result.extend(res.items);
         next_page = res.next_page_token;
         println!("query again with tk:");
-        if next_page.is_none() { break;}
+        if next_page.is_none() {
+            break;
+        }
     }
     result
 }
 
-fn get_subscriptions_for_me(t : &oauth2::Token) -> Vec<YoutubeItem<YoutubeSubscription>> {
+fn get_subscriptions_for_me(t: &oauth2::Token) -> Vec<YoutubeItem<YoutubeSubscription>> {
     query(t, SUB_URL)
 }
 
-fn construct_subscription(s : YoutubeItem<YoutubeSubscription>) -> NewSubscription {
+fn construct_subscription(s: YoutubeItem<YoutubeSubscription>) -> NewSubscription {
     let item = s.snippet.unwrap();
-    NewSubscription { channelname : item.subscription_title, uploadplaylist : String::from("test playlist"), thumbnail : item.thumbnails.default.thmburl, description : item.sdescription}
+    NewSubscription {
+        channelname: item.subscription_title,
+        uploadplaylist: String::from("test playlist"),
+        thumbnail: item.thumbnails.default.thmburl,
+        description: item.sdescription,
+    }
 }
 
-pub fn get_subs(t : &oauth2::Token, db : &Mutex<SqliteConnection>, update_subs : bool) -> Vec<Subscription> {
+pub fn get_subs(t: &oauth2::Token,
+                db: &Mutex<SqliteConnection>,
+                update_subs: bool)
+                -> Vec<Subscription> {
     use schema::subscriptions::dsl::*;
     use schema::subscriptions;
 
-    let dbconn : &SqliteConnection = &db.lock().unwrap();
+    let dbconn: &SqliteConnection = &db.lock().unwrap();
     if update_subs {
         delete(subscriptions::table).execute(dbconn);
-        
+
         let ytsubs = get_subscriptions_for_me(t);
         let it = ytsubs.into_iter();
         let subs = it.map(construct_subscription).collect::<Vec<NewSubscription>>();
-        insert(&subs).into(subscriptions::table)
+        insert(&subs)
+            .into(subscriptions::table)
             .execute(dbconn);
     }
     subscriptions.load::<Subscription>(dbconn).unwrap()
