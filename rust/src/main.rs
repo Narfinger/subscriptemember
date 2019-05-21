8c4d85412a9de557a45c52272e256a8e3ef6bc17
Narfinger <Narfinger@users.noreply.github.com>
2019-05-21 22:32:22 +0530

    partial stuff

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,4 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use]
-extern crate rocket;
+extern crate actix_web;
 extern crate serde;
 extern crate reqwest;
 extern crate chrono;
@@ -13,7 +10,6 @@ extern crate serde_json;
 extern crate tokio_core;
 extern crate oauth2;
 extern crate handlebars;
-extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
 #[macro_use]
@@ -33,6 +29,9 @@ pub mod youtube_video;
 pub mod subs_and_video;
 pub mod giantbomb_video;
 
+use actix_web::{App, HttpRequest, HttpResponse, Responder, State, Path};
+use actix_web::http::{StatusCode, ContentEncoding, Method, header};
+
 use std::sync::{RwLock, Mutex};
 use std::thread;
 use std::sync::mpsc;
@@ -43,10 +42,7 @@ use chrono::NaiveDateTime;
 use diesel::sqlite::SqliteConnection;
 use diesel::r2d2::Pool;
 use diesel::r2d2::ConnectionManager;
-use rocket::request::{State, Form, FromFormValue};
-use rocket::response::Redirect;
-use rocket::response::content::Content;
-use rocket::http::ContentType;
+
 use url::Url;
 use crate::subs_and_video::{GBKey, get_lastupdate_in_unixtime};
 use crate::youtube_oauth::{Expireing, setup_oauth};
@@ -54,66 +50,57 @@ use crate::youtube_oauth::{Expireing, setup_oauth};
 const APP_INFO: AppInfo = AppInfo{name: "subscriptemember", author: "narfinger"};
 const PREFS_KEY: &'static str = "subscriptemember_prefs";
 
-struct TK(RwLock<youtube_oauth::Token>);
-struct GBTK(GBKey);
-struct DB(Pool<ConnectionManager<SqliteConnection>>);
-struct HB(handlebars::Handlebars);
-struct UpdatingVideos(Mutex<()>);
-#[derive(Clone)]
-struct MPSC(mpsc::SyncSender<()>);
 
-struct MyURL(reqwest::Url);
-#[derive(FromForm)]
-struct AddForm {
-    url: MyURL,
-}
-
-impl<'v> FromFormValue<'v> for MyURL {
-    type Error = reqwest::UrlError;
-    fn from_form_value(v: &'v rocket::http::RawStr) -> Result<Self, Self::Error> {
-        let mut nv = v.replace("%3A", ":");
-        nv = nv.replace("%2F", "/");
-        let parsed = Url::from_str(&nv);
-        match parsed {
-            Ok(x) => Ok(MyURL(x)),
-            Err(x) => Err(x),
-        }
-    }
+struct AppState {
+    tk: RwLock<youtube_oauth::Token>,
+    gbtk: GBKey,
+    db: Pool<ConnectionManager<SqliteConnection>>,
+    hb: handlebars::Handlebars,
+    updating_videos: Mutex<()>,
+    mpsc: mpsc::SyncSender<()>,
 }
 
 fn setup_gbkey() -> GBKey {
     GBKey { key: include_str!("../client_secret_gb.json").to_string() }
 }
 
-#[get("/updateSubs")]
-fn update_subs(tk: State<TK>, db: State<DB>) -> Redirect {
+
+fn update_subs(state: State<AppState>) -> impl Responder {
+    let tk = state.tk;
+    let db = state.db;
     if tk.0.read().unwrap().expired() {
         let mut rwtk = tk.0.write().unwrap();
         *rwtk = setup_oauth().expect("Error getting fresh token");
     }
     let cl = reqwest::Client::new();
     youtube_subscriptions::get_subs(&tk.0.read().unwrap(), &db.0, &cl, true);
-    Redirect::to("/subs")
+
+    Ok(HttpResponse::build(StatusCode::TEMPORARY_REDIRECT)
+            .header(header::LOCATION, "/subs")
+            .finish())
 }
 
-#[get("/subs")]
-fn subs(tk: State<TK>, db: State<DB>, hb: State<HB>) -> Content<String> {
+fn subs(state: State<AppState>) -> String {
+    let tk = state.tk;
+    let db = state.db;
+    let hb = state.hb;
+
     let cl = reqwest::Client::new();
     let sub = youtube_subscriptions::get_subs(&tk.0.read().unwrap(), &db.0, &cl, false);
     let data = json!({
         "subs": sub,
         "numberofsubs": sub.len(),
     });
-    Content(ContentType::HTML, hb.0.render("subs", &data).unwrap())
+    hb.0.render("subs", &data).unwrap()
 }
 
-#[get("/updateVideos")]
-fn update_videos(tk: State<TK>,
-                 gbtk: State<GBTK>,
-                 db: State<DB>,
-                 ch: State<MPSC>,
-                 upv: State<UpdatingVideos>)
-                 -> Redirect {
+fn update_videos(state: State<AppState>) -> impl Responder {
+    let tk = state.tk;
+    let gbtk = state.gbtk;
+    let db = state.db;
+    let ch = state.mpsc;
+    let upv = state.updating_videos;
+
     if upv.0.try_lock().is_ok() {
         if tk.0.read().unwrap().expired() {
             let mut rwtk = tk.0.write().unwrap();
@@ -135,35 +122,22 @@ fn update_videos(tk: State<TK>,
             nch.0.send(());
         });
     }
-    Redirect::to("/")
+    Ok(HttpResponse::build(StatusCode::TEMPORARY_REDIRECT)
+            .header(header::LOCATION, "/")
+            .finish())
 }
 
-#[get("/delete/<vid>")]
-fn delete(vid: String, db: State<DB>) -> Redirect {
+fn delete(vid: Path<(String,String)>, req: HttpRequest) -> impl Responder {
+    let db = req.state().db;
     youtube_video::delete_video(&db.0, &vid);
-    Redirect::to("/")
+    Ok(HttpResponse::build(StatusCode::TEMPORARY_REDIRECT)
+            .header(header::LOCATION, "/")
+            .finish())
 }
 
-// #[get("/sockettest")]
-// fn sockettest(sc: State<MPSC>) {
-//     sc.0.send(());
-// }
-
-// #[get("/static/<file..>")]
-// fn static_files(file: PathBuf) -> Option<NamedFile> {
-//     NamedFile::open(Path::new("static/").join(file)).ok()
-// }
-
-#[post("/addurl", data="<form>")]
-fn addurl(form: Form<AddForm>) -> Redirect {
-    panic!("not implemented")
-    //let url = &form.get().url.0;
-    //println!("found something {:?}", url);
-    //Redirect::to("/")
-}
-
-#[get("/small")]
-fn small(db: State<DB>, hb: State<HB>) -> Content<String> {
+fn index(req: HttpRequest) -> String {
+    let db = req.state().db;
+    let hb = req.state().hb;
     let vids = youtube_video::get_videos(&db.0);
 
     let lastrefreshed =
@@ -178,29 +152,7 @@ fn small(db: State<DB>, hb: State<HB>) -> Content<String> {
         "numberofvideos": numberofvideos,
         "totaltime": totaltime,
     });
-    Content(ContentType::HTML,
-            hb.0.render("index", &data).map_err(|err| err.to_string()).unwrap())
-}
-
-
-#[get("/")]
-fn index(db: State<DB>, hb: State<HB>) -> Content<String> {
-    let vids = youtube_video::get_videos(&db.0);
-
-    let lastrefreshed =
-        format!("{}", NaiveDateTime::from_timestamp(get_lastupdate_in_unixtime(&db.0), 0)
-                                .format("%H:%M:%S %d.%m.%Y"));
-    let numberofvideos = vids.len();
-    let totaltime: i64 = vids.iter().map(|v| v.duration).sum();
-
-    let data = json!({
-        "vids": vids,
-        "lastrefreshed": lastrefreshed,
-        "numberofvideos": numberofvideos,
-        "totaltime": totaltime,
-    });
-    Content(ContentType::HTML,
-            hb.0.render("index", &data).map_err(|err| err.to_string()).unwrap())
+    hb.0.render("index", &data).map_err(|err| err.to_string()).unwrap()
 }
 
 fn video_time(h: &Helper, _: &Handlebars, _: &Context, _: &mut RenderContext, out: &mut Output) -> Result<(), RenderError> {
@@ -232,21 +184,16 @@ fn video_duration(h: &Helper, _: &Handlebars, _: &Context, _: &mut RenderContext
     Ok(())
 }
 
-// fn video_url(h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
-//     let param = h.param(0).unwrap().value().to_string().replace("\"", "");
-//     let url = "https://www.youtube.com/watch?v=".to_string() + param.as_str();
-//     try!(rc.writer.write_all(url.into_bytes().as_ref()));
-//     Ok(())
-// }
-
-#[get("/static/datatables.css")]
-fn static_datatablescss() -> Content<String> {
-    Content(ContentType::CSS, String::from(include_str!("../static/datatables.css")))
+fn static_datatablescss() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(include_str!("../static/datatables.css"))
 }
 
-#[get("/static/datatables.js")]
-fn static_datatablesjs() -> Content<String> {
-    Content(ContentType::JavaScript, String::from(include_str!("../static/datatables.js")))
+fn static_datatablesjs() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/javascript")
+        .body(include_str!("../static/datatables.js"))
 }
 
 fn main() {
@@ -273,8 +220,8 @@ fn main() {
 
     println!("Setting up channels");
     let (sender, receiver) = mpsc::sync_channel::<()>(2);
-    let chstruct = MPSC(sender);
-    
+    //let chstruct = MPSC(sender);
+
     {
         println!("Starting websockets");
         let ws = ws::WebSocket::new(|_| {
@@ -283,7 +230,7 @@ fn main() {
             }
         }).unwrap();
         let broadcaster = ws.broadcaster();
-        
+
         thread::spawn(move || {
             if receiver.recv().is_ok() {
                 broadcaster.send(ws::Message::text("refresh please"))
@@ -291,26 +238,26 @@ fn main() {
                 broadcaster.send(ws::Message::text("disconnected"))
             }
         });
-        
+
         thread::spawn(|| ws.listen("127.0.0.1:3012"));
     }
 
-    
+
     //let cl = reqwest::Client::new().expect("Error in creating connection pool");
-    
+
     println!("Do transaction for update!");
 
     println!("Starting server");
     let oauth = setup_oauth().expect("Problem with oauth");
-    rocket::ignite()
-        .mount("/",
-               routes![update_subs, subs, update_videos, delete,
-                         static_datatablescss, static_datatablesjs, addurl, small, index])
-        .manage(TK(RwLock::new(oauth)))
-        .manage(GBTK(setup_gbkey()))
-        .manage(DB(pool))
-        .manage(HB(hb))
-        .manage(UpdatingVideos(Mutex::new(())))
-        .manage(chstruct)
-        .launch();
+
+    App::with_state(AppState {
+        tk: RwLock::new(oauth),
+        gbtk: setup_gbkey(),
+        db: pool,
+        hb: hb,
+        updating_videos: Mutex::new(),
+        mpsc: sender,
+    })
+    .resource("/", |r| r.method(Method::GET).f(index))
+    .finish()
 }
